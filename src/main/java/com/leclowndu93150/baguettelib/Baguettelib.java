8a9b9be6f062d19feb06c499cf9a0c4ca7b43cd7
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-08-21 01:29:07 +0200

    fix an oopsie

--- src/main/java/com/leclowndu93150/baguettelib/Baguettelib.java
+++ src/main/java/com/leclowndu93150/baguettelib/Baguettelib.java
@@ -1,9 +1,6 @@
 package com.leclowndu93150.baguettelib;
 
-import com.leclowndu93150.baguettelib.event.entity.CreativeFlightEvent;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
@@ -14,12 +11,12 @@ public class Baguettelib
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Baguettelib(){
-        MinecraftForge.EVENT_BUS.addListener(this::onCreativeFlight);
+        //MinecraftForge.EVENT_BUS.addListener(this::onCreativeFlight);
     }
 
-    @SubscribeEvent
-    public void onCreativeFlight(CreativeFlightEvent.Toggle event){
-        event.setCanceled(true);
-        event.setFlightState(false);
-    }
+//    @SubscribeEvent
+//    public void onCreativeFlight(CreativeFlightEvent.Toggle event){
+//        event.setCanceled(true);
+//        event.setFlightState(false);
+//    }
 }
