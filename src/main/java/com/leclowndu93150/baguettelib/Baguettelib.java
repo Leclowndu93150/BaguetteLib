e9dc203f92d541173d55dec7b395cf6d20240ff2
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-09-25 11:10:41 +0200

    change priority mixin to fix https://github.com/Leclowndu93150/BaguetteLib/issues/4

--- src/main/java/com/leclowndu93150/baguettelib/Baguettelib.java
+++ src/main/java/com/leclowndu93150/baguettelib/Baguettelib.java
@@ -5,8 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(Baguettelib.MODID)
-public class Baguettelib
-{
+public class Baguettelib {
     public static final String MODID = "baguettelib";
     private static final Logger LOGGER = LogUtils.getLogger();
 
--- src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
+++ src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
@@ -6,14 +6,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
-@Mixin(value = ItemEntity.class, priority = 500)
+@Mixin(value = ItemEntity.class, priority = 727)
 public class ItemEntityPickupMixin {
 
 
