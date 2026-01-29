449fa1a451de701593ba65a4b17e89e2ecdfb62c
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-01-29 09:19:17 +0100

    close https://github.com/Leclowndu93150/BaguetteLib/issues/8

--- src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
+++ src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -58,6 +59,7 @@ public class ItemEntityPickupMixin {
         }
     }
 
+    @Unique
     private int findTargetSlot(Player player, ItemStack itemStack) {
         var inventory = player.getInventory();
 
