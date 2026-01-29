e9f700788646104841d17e24a3c1bc2185d8237d
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-01-29 09:13:26 +0100

    fix little tiny design flaw 2

--- src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
+++ src/main/java/com/leclowndu93150/baguettelib/mixin/ItemEntityPickupMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -57,6 +58,7 @@ public class ItemEntityPickupMixin {
         }
     }
 
+    @Unique
     private int findTargetSlot(Player player, ItemStack itemStack) {
         var inventory = player.getInventory();
 
